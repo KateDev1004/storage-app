e2685d0daed7fe6ce4a04fbd20354bacc3127060
JsTsDev Pinky <RosemaryR784@outlook.com>
2024-09-26 00:17:33 +0200

    added footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,18 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="logo">YourLogo</div>
-        <ul>
-          <li>Contact Us</li>
-          <li>Privacy Policy</li>
-          <li>Terms of Service</li>
-        </ul>
-        <div className="social-icons">
-          <img src="/assets/images/facebook-icon.png" alt="Facebook" />
-          <img src="/assets/images/twitter-icon.png" alt="Twitter" />
-          <img src="/assets/images/instagram-icon.png" alt="Instagram" />
-        </div>
+      <div className="footer-content text-center py-4">
+        @2024 Datura.ai. All rights reserved
       </div>
     </footer>
   );
